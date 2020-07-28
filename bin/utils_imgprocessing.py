347c0e260788047061386e93ab4fa6ff7b3dc699
Deiner Zapata <deinerzapata@gmail.com>
2020-07-27 19:26:08 -0500

    Update utils_imgprocessing.py

--- bin/utils_imgprocessing.py
+++ bin/utils_imgprocessing.py
@@ -158,3 +158,15 @@ def split_labels(img, labels):
     for filter_value in list_labels:
         tmp_img = filter_label(img, labels, filter_value)
         cv2.imwrite("label_{0:02d}.jpg".format(filter_value), tmp_img)
+#######################################################################################
+def auto_canny(image, sigma=0.33):
+    # compute the median of the single channel pixel intensities
+    v = np.median(image)
+
+    # apply automatic Canny edge detection using the computed median
+    lower = int(max(0, (1.0 - sigma) * v))
+    upper = int(min(255, (1.0 + sigma) * v))
+    edged = cv2.Canny(image, lower, upper)
+
+    # return the edged image
+    return edged
