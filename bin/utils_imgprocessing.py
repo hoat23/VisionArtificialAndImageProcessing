438dfab4f54b766aefaa2bcf48226ce65227baaa
Deiner Zapata <deinerzapata@gmail.com>
2020-07-27 23:27:05 -0500

    Update utils_imgprocessing.py

--- bin/utils_imgprocessing.py
+++ bin/utils_imgprocessing.py
@@ -79,6 +79,18 @@ def plot_img(img):
   f = plt.figure(figsize=(width, height))
   plt.imshow(img)
 
+def plot_list_img(list_img, rows=1, cols=2):
+  size = rows * cols 
+  if size < len(list_img):
+    rows = int(len(list_img)/cols)
+  for num, img in enumerate(list_img):
+    title = "Img{0:02d}".format(num)
+    plt.subplot(rows,cols,num+1)
+    plt.title(title)
+    plt.axis('on')
+    plt.imshow(img)
+    #print("[{0:02d} | {1:02d}] [{2}]".format(rows,cols, title))
+
 #######################################################################################
 	
 """
