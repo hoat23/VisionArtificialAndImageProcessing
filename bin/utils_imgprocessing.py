144e98ad2829c15c7ab175a2f430d24c7a08e10d
Deiner Zapata <6503057+hoat23@users.noreply.github.com>
2020-08-21 21:26:44 -0500

    Update utils_imgprocessing.py

--- bin/utils_imgprocessing.py
+++ bin/utils_imgprocessing.py
@@ -210,3 +210,33 @@ def auto_canny(image, sigma=0.33):
 
     # return the edged image
     return edged
+
+def skeletonize(image, size, structuring=cv2.MORPH_RECT):
+    # determine the area (i.e. total number of pixels in the image),
+    # initialize the output skeletonized image, and construct the
+    # morphological structuring element
+    area = image.shape[0] * image.shape[1]
+    skeleton = np.zeros(image.shape, dtype="uint8")
+    elem = cv2.getStructuringElement(structuring, size)
+
+    # keep looping until the erosions remove all pixels from the
+    # image
+    while True:
+        # erode and dilate the image using the structuring element
+        eroded = cv2.erode(image, elem)
+        temp = cv2.dilate(eroded, elem)
+
+        # subtract the temporary image from the original, eroded
+        # image, then take the bitwise 'or' between the skeleton
+        # and the temporary image
+        temp = cv2.subtract(image, temp)
+        skeleton = cv2.bitwise_or(skeleton, temp)
+        image = eroded.copy()
+
+        # if there are no more 'white' pixels in the image, then
+        # break from the loop
+        if area == area - cv2.countNonZero(image):
+            break
+
+    # return the skeletonized image
+    return skeleton
